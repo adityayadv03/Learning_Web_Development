8eb6899ddaac8f45b170941c4a664d414b7c6620
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-02-28 19:34:33 +0530

    Learn about innerHTML

--- JS_Basics/Basic_Chrome_Extension/script.js
+++ JS_Basics/Basic_Chrome_Extension/script.js
@@ -2,16 +2,26 @@
 //     console.log("Button Clicked")
 // }
 
-let myLeads=[]
+let myLeads=["www.abc.com","www.def.com","www.ghi.com"]
 const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEL=document.getElementById("ul-el")
 
 inputBtn.addEventListener("click" , function(){
-    // console.log("Button clicked from addEventListener")
     myLeads.push(inputEl.value)
+    console.log(myLeads)
 })
 
+let listItems=""
 for(let i=0 ; i<myLeads.length ; i+=1){
-    ulEL.textContent+=myLeads[i]+" "
+    listItems+="<li>"+myLeads[i]+"</li>"
 }
+ulEL.innerHTML=listItems
+
+// innerHTML Practice
+// let divEL=document.getElementById("container")
+// function buy(){
+//     divEL.innerHTML+="<p>Thank you for buying!</p>"
+// }
+
+// divEL.innerHTML="<button onclick='buy()'>But It!</button>"
