89d589385434d114e114466e72642eb090ce47fe
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-02-29 19:03:42 +0530

    Some Changes

--- JS_Basics/Basic_Chrome_Extension/script.js
+++ JS_Basics/Basic_Chrome_Extension/script.js
@@ -2,21 +2,30 @@
 //     console.log("Button Clicked")
 // }
 
-let myLeads=["www.abc.com","www.def.com","www.ghi.com"]
+let myLeads=[]
 const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEL=document.getElementById("ul-el")
 
 inputBtn.addEventListener("click" , function(){
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    renderLeads()
+    inputEl.value=''
 })
 
-let listItems=""
-for(let i=0 ; i<myLeads.length ; i+=1){
-    listItems+="<li>"+myLeads[i]+"</li>"
+function renderLeads(){
+    let listItems=""
+    for(let i=0 ; i<myLeads.length ; i+=1){
+        // listItems+="<li><a href='"+myLeads[i]+"' target='_blank'>"+myLeads[i]+"</a></li>"
+        listItems+=`
+        <li>
+            <a href='${myLeads[i]}' target='_blank'>
+                ${myLeads[i]}
+            </a>
+        </li>`
+    }
+    ulEL.innerHTML=listItems
 }
-ulEL.innerHTML=listItems
 
 // innerHTML Practice
 // let divEL=document.getElementById("container")
