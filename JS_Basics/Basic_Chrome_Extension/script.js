435ae39bb549e24b46b1d2fa4257412453956607
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-03-17 10:12:19 +0530

    Completed the Extension

--- JS_Basics/Basic_Chrome_Extension/script.js
+++ JS_Basics/Basic_Chrome_Extension/script.js
@@ -4,21 +4,31 @@ const inputEl=document.getElementById("input-el")
 const inputBtn=document.getElementById("input-btn")
 const ulEL=document.getElementById("ul-el")
 const delEL=document.getElementById("delete-btn")
+const tabBtn=document.getElementById("tab-btn")
+
+tabBtn.addEventListener("click" , function click(){
+    chrome.tabs.query({active:true,currentWindow:true}, function(tabs){
+        console.log(tabs)
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads",JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
 
 delEL.addEventListener("dblclick",function clear(){
     localStorage.clear()
     myLeads=[]
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads(){
+function render(leads){
     let listItems=""
-    for(let i=0 ; i<myLeads.length ; i+=1){
+    for(let i=0 ; i<leads.length ; i+=1){
         // listItems+="<li><a href='"+myLeads[i]+"' target='_blank'>"+myLeads[i]+"</a></li>"
         listItems+=`
         <li>
-            <a href='${myLeads[i]}' target='_blank'>
-                ${myLeads[i]}
+            <a href='${leads[i]}' target='_blank'>
+                ${leads[i]}
             </a>
         </li>`
     }
@@ -29,14 +39,14 @@ inputBtn.addEventListener("click" , function(){
     myLeads.push(inputEl.value)
     inputEl.value=''
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
     console.log(localStorage.getItem("myLeads"))
 })
 
-const lead=[]
+let lead=[]
 lead=JSON.parse(localStorage.getItem("myLeads"))
 console.log(lead)
 if(lead){
     myLeads=lead
-    renderLeads()
+    render(myLeads)
 }
