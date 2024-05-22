214ae80158af8b2df602bb4e487c733ddff46dfa
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-05-22 21:40:33 +0530

    Providing Description to the code

--- JS_Basics/Basic_Loops/for_of_loop.js
+++ JS_Basics/Basic_Loops/for_of_loop.js
@@ -1,5 +1,7 @@
-const obj=require('readline-sync');
-let string=obj.question("Enter Any String :- ");
-for(let ch of string){
-    console.log("Character :- ",ch);
+//Author :- Aditya Yadav
+//A basic Program implementing for of loop in javascript
+const obj=require('readline-sync'); //Taking input from the terminal using readline-sync module of node js
+let string=obj.question("Enter Any String :- "); //Taking input with the help of question function of the module
+for(let ch of string){ //Iterating with the help of for of loop
+    console.log("Character :- ",ch); //Printing each character in new line
 }
