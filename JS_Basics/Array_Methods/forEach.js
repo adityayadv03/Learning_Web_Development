7214f57dc11c5edd18b7e993244719afdb894e2e
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-05-24 20:48:02 +0530

    forEach Method in Js

---
+++ JS_Basics/Array_Methods/forEach.js
@@ -0,0 +1,6 @@
+//Author :- Aditya Yadav
+//Basic implementation of forEach method in javascript
+let arr=[5,2,1,8,5,3,4,5];
+arr.forEach((val)=>{ //the method take a callback function as a argument
+    console.log(val);
+})
