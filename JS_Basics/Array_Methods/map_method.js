e76e08c12fcce9795c160a7380af880de7da6333
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-05-25 20:26:08 +0530

    Map Method in Array Javascript

---
+++ JS_Basics/Array_Methods/map_method.js
@@ -0,0 +1,7 @@
+//Author :- Aditya Yadav
+//Basic Implementation of map function
+//Generally Used when a new copy of array is require as it create a new array from the previous array
+let arr=[5,2,1,8,5,3,4,5];
+let newarr=arr.map((val)=>{
+    return val*2;
+});
