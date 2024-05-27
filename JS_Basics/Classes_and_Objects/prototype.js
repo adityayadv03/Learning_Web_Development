34020a8228b096b2ffe61d2d7c1d94b20bed45f7
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-05-27 21:40:48 +0530

    Adding Some Description

--- JS_Basics/Classes_and_Objects/prototype.js
+++ JS_Basics/Classes_and_Objects/prototype.js
@@ -1,11 +1,15 @@
-const employee={
-    calcTax(){
+//Author :- Aditya Yadav
+//Basic Implementation of prototype object in Javascript
+const employee={ //Creating a object
+    calcTax(){ //Another way of creating function inside a object
         console.log("Tax rate is 10%");
     }
 }
-const aditya={
+const aditya={ //Creating another object
     salary:150000
 }
-aditya.__proto__=employee;
+aditya.__proto__=employee; //Adding the first object to the prototype of the second object
 
-aditya.calcTax();
+console.log(aditya.__proto__); //Here the second object have the function calcTax without defining it again in second object
+
+aditya.calcTax(); //Calling the calcTax function with the help of second function
