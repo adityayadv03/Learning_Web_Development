d5cebe38a5108889df69da56d819f6e3d4ab7c16
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-01 21:00:53 +0530

    Error Handling Example

---
+++ JS_Basics/Error_Handling/tryandcatch.js
@@ -0,0 +1,9 @@
+//Author :- Aditya Yadav
+//Implementing Error Handling in Js using try and catch block
+let a=0;
+let b=5;
+try{
+    console.log("Division :- "+(a/c));
+}catch(err){
+    console.log("Error :- "+err);
+}
