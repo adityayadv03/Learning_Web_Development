6fb4a91ef549e1be54913410f998b2a77ea30ff0
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-02 19:18:39 +0530

    Adding Description in file

--- JS_Basics/Callback_Promises/Callback.js
+++ JS_Basics/Callback_Promises/Callback.js
@@ -1,7 +1,10 @@
-function sum(a,b){
+//Author :- Aditya Yadav
+//Callback Example Implementation
+//Callback is the function as a argument to another function
+function sum(a,b){ //A normal function performing addition
     console.log(a+b);
 }
-function calculator(a,b,callbackfunction){
-    callbackfunction(a,b);
+function calculator(a,b,callbackfunction){ //another function taking another function as parameter
+    callbackfunction(a,b); //calling the another function with parameter
 }
-calculator(1,2,sum);
+calculator(1,2,sum); //calling the function calculator and passing sum(a function) as a argument of the first function calculator
