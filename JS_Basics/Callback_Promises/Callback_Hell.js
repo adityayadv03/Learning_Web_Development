995a356659e0383f45ac479631840f3a3b7ab09f
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-03 21:06:32 +0530

    Explaining Code Block

--- JS_Basics/Callback_Promises/Callback_Hell.js
+++ JS_Basics/Callback_Promises/Callback_Hell.js
@@ -1,4 +1,7 @@
-function getData(dataId,getNextData){
+// Author :- Aditya Yadav
+//Callback Hell Example
+//Callback Hell is a problem Creating a difficulty of understanding code block due to callback , it is difficult for the programmer to understand another programmer code block
+function getData(dataId,getNextData){ //function which is use to implement the function part 
     setTimeout(()=>{
         console.log("Data :- ",dataId);
         if(getNextData){
@@ -8,16 +11,16 @@ function getData(dataId,getNextData){
 }
 
 console.log("Getting Data_1 ...");
-getData(1,()=>{
+getData(1,()=>{ //First Function call with arrow function as callback
     console.log("Getting Data_2 ...");
-    getData(2,()=>{
+    getData(2,()=>{ //Second Function call with arrow function as callback
         console.log("Getting Data_3 ...");
-        getData(3,()=>{
+        getData(3,()=>{ //Third Function call with arrow function as callback
             console.log("Getting Data_4 ...");
-            getData(4,()=>{
+            getData(4,()=>{ //Fourth Function call with arrow function as callback
                 console.log("Getting Data_5 ...");
-                getData(5);
-            })
-        })
-    })
-})
+                getData(5); //Fifth Function call with arrow function as callback
+            });
+        });
+    });
+});
