7a5f94b4905b2c41550a68acfc8dd9adc0902290
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-05 20:01:43 +0530

    Explaining Code block

--- JS_Basics/Callback_Promises/Promise_Chaining.js
+++ JS_Basics/Callback_Promises/Promise_Chaining.js
@@ -1,4 +1,6 @@
-function func1(){
+//Author :- Aditya Yadav
+//Implementing the Promise Chaining In JavaScript
+function func1(){ //A Function returning a Promise
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             console.log("Data_1");
@@ -7,7 +9,7 @@ function func1(){
     });
 };
 
-function fun2(){
+function fun2(){ //A Function returning a Promise
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             console.log("Data_2");
@@ -16,8 +18,8 @@ function fun2(){
     });
 };
 
-console.log("Fetching Data_1...");
-func1().then((res)=>{
-    console.log("Fetching Data_2 ...");
+console.log("Fetching Data_1..."); //Console message for fetching the data
+func1().then((res)=>{ // then is a inbuilt function helping the programmer to perform any task after the promise is given by any api or function
+    console.log("Fetching Data_2 ..."); //After the first promise the second function is called 
     fun2().then((res)=>{});
 });
