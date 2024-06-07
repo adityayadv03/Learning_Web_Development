d4c76d5fcea66ee8cc5b718357c9c1cd0626abca
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-07 21:10:02 +0530

    Explaining the Code

--- JS_Basics/Async_await/asyn_await.js
+++ JS_Basics/Async_await/asyn_await.js
@@ -1,4 +1,7 @@
-function api(){
+//Author :- Aditya Yadav
+//Example of async and await keyword in Javascript
+//async and await are the two keyword used  understand tha complex code of promise chaining and callback hell with only two keyword
+function api(){ //Function which is called 
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             console.log("Weather Data . ");
@@ -7,7 +10,8 @@ function api(){
     });
 };
 
-async function getWeatherData(){
-    await api();
+async function getWeatherData(){ //A Async function necessary for using a await keyword
+    await api(); //Whenever the await is used the code will wait for the completion of the task and halt all other execution of task
     await api();
 }
+getWeatherData();
