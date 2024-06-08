7e9f87829a4748124560fe6363a0c9f470617e1e
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-08 10:56:33 +0530

    Explain the code

--- JS_Basics/Async_await/asyn_await_2.js
+++ JS_Basics/Async_await/asyn_await_2.js
@@ -1,5 +1,6 @@
+//Author :- Aditya Yadav
 //Another Example of Async and await code
-function getData(dataId){
+function getData(dataId){ //Function which is called during the data fetching
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
             console.log("Data :- "+dataId);
@@ -8,10 +9,10 @@ function getData(dataId){
     });
 }
 
-async function getAllData(){
-    await getData(1);
+async function getAllData(){ //A Async function necessary for using a await keyword
+    await getData(1); //Whenever the await is used the code will wait for the completion of the task and halt all other execution of task
     await getData(2);
     await getData(3);
 }
 
-getAllData();
+getAllData(); //Function call to execute the all the task define in the function
