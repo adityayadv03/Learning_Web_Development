db906c358cfa03c52f39c59e12c43f10893db605
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-06-11 20:31:41 +0530

    IIFE Function in Js

---
+++ JS_Basics/Working_with_API/IIFE.js
@@ -0,0 +1,7 @@
+//Author :- Aditya Yadav
+//IIFE function stand for Immediately Invoked Function Expression means it a function that is called immediately as soon as it defined
+//Its is generally used in place where the function has to be called onced and has to be called
+
+(function printHello(){
+    console.log("Hello User . ");
+})();
